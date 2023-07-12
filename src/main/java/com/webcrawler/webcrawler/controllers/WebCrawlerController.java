5b4756ec6821999abcdad7f8667b300f421c0b83
alexandrospavlidis <alexandrospavlidis@skyscanner.net>
2023-07-12 22:14:47 +0100

    add validation for ulr

--- src/main/java/com/webcrawler/webcrawler/controllers/WebCrawlerController.java
+++ src/main/java/com/webcrawler/webcrawler/controllers/WebCrawlerController.java
@@ -1,5 +1,6 @@
 package com.webcrawler.webcrawler.controllers;
 
+import com.webcrawler.webcrawler.service.ValidationService;
 import com.webcrawler.webcrawler.service.WebCrawlerService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
@@ -12,13 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebCrawlerController {
 
 private final WebCrawlerService crawlerService;
+private final ValidationService validationService;
 
-public WebCrawlerController(WebCrawlerService crawlerService) {
+public WebCrawlerController(WebCrawlerService crawlerService, ValidationService validationService) {
 	this.crawlerService = crawlerService;
+  this.validationService = validationService;
 }
 
 @GetMapping(value = "/crawl")
 public ResponseEntity<List<String>> crawlUrl(@RequestParam String url,@RequestParam(required = false) Integer limit){
+  List<String> errorMessages = validationService.validate(url);
+
+  if(!errorMessages.isEmpty()){
+    return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+  }
+
 	return new ResponseEntity<>(crawlerService.crawl(url, limit), HttpStatus.OK);
 }
 
---
+++ src/main/java/com/webcrawler/webcrawler/service/ValidationService.java
@@ -0,0 +1,31 @@
+package com.webcrawler.webcrawler.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidationService {
+
+
+  public List<String> validate(String url) {
+    ArrayList<String> errorMessages = new ArrayList<>();
+
+    if(isNotValidUrl(url)){
+      errorMessages.add("The url provided is invalid");
+    }
+
+    return errorMessages;
+  }
+
+
+  public static boolean isNotValidUrl(String url) {
+    String regex = "^(https)://([\\w.-]+)(:\\d+)?(/\\S*)?$";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(url);
+    return !matcher.matches();
+  }
+
+}
