6345265d7601b7d547ef16c44ef47a0c624a9e07
alexandrospavlidis <alexandrospavlidis@skyscanner.net>
2023-07-16 13:52:16 +0100

    configure xray

--- src/main/java/com/webcrawler/webcrawler/web/WebConfig.java
+++ src/main/java/com/webcrawler/webcrawler/web/WebConfig.java
@@ -2,13 +2,14 @@ package com.webcrawler.webcrawler.web;
 
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.strategy.SegmentNamingStrategy;
-import org.springframework.context.annotation.Configuration;
+import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebConfig {
   @Bean
-  public AWSXRayServletFilter TracingFilter() {
+  public Filter TracingFilter() {
     return new AWSXRayServletFilter(SegmentNamingStrategy.dynamic("webcrawl"));
   }
 }
